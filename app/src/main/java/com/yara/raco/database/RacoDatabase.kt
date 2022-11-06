1409e0d59358ddd0879190ae3b225b8be490bb1f
hegocre <15657088+hegocre@users.noreply.github.com>
2022-11-06 17:30:11 +0100

    Use a single database

--- app/src/main/java/com/yara/raco/database/RacoDatabase.kt
+++ app/src/main/java/com/yara/raco/database/RacoDatabase.kt
@@ -1,28 +1,29 @@
-package com.yara.raco.database.subject
+package com.yara.raco.database
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.yara.raco.database.subject.SubjectDAO
 import com.yara.raco.model.subject.Subject
 
-@Database(entities = [Subject::class], version = 8, exportSchema = false)
-abstract class SubjectDatabase : RoomDatabase () {
-    abstract val subjectDatabaseDAO: SubjectDatabaseDAO
+@Database(entities = [Subject::class], version = 1, exportSchema = false)
+abstract class RacoDatabase : RoomDatabase() {
+    abstract val subjectDAO: SubjectDAO
 
     companion object {
         @Volatile
-        private var INSTANCE: SubjectDatabase? = null
+        private var INSTANCE: RacoDatabase? = null
 
-        fun getInstance(context: Context): SubjectDatabase {
+        fun getInstance(context: Context): RacoDatabase {
             synchronized(this) {
                 var instance = INSTANCE
 
                 if (instance == null) {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
-                        SubjectDatabase::class.java,
-                        "subjects.db"
+                        RacoDatabase::class.java,
+                        "raco.db"
                     )
                         .fallbackToDestructiveMigration()
                         .build()
