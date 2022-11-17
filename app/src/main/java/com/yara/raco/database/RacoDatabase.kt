02959df994a773ba50fe1db8a44c9e63c224caa5
IFibla <76115905+ifibla@users.noreply.github.com>
2022-11-17 14:14:27 +0100

    Add Grade Model in Database
    Signed-off-by: Ignasi <ignasi.fibla@upc.edu>

--- app/src/main/java/com/yara/raco/database/RacoDatabase.kt
+++ app/src/main/java/com/yara/raco/database/RacoDatabase.kt
@@ -5,14 +5,16 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.yara.raco.database.file.FileDAO
+import com.yara.raco.database.grades.GradesDAO
 import com.yara.raco.database.notice.NoticeDAO
 import com.yara.raco.database.subject.SubjectDAO
 import com.yara.raco.model.files.File
 import com.yara.raco.model.notices.Notice
 import com.yara.raco.model.subject.Subject
+import com.yara.raco.model.grades.Grade
 
 @Database(
-    entities = [Subject::class, Notice::class, File::class],
+    entities = [Subject::class, Notice::class, File::class, Grade::class],
     version = 2,
     exportSchema = false
 )
@@ -20,6 +22,7 @@ abstract class RacoDatabase : RoomDatabase() {
     abstract val subjectDAO: SubjectDAO
     abstract val noticeDAO: NoticeDAO
     abstract val fileDAO: FileDAO
+    abstract val gradesDAO: GradesDAO
 
     companion object {
         @Volatile
