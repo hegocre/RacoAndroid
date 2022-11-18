5998ed9b1fffb4cfe92e70e01dbfdd6b67b17bd7
IFibla <76115905+ifibla@users.noreply.github.com>
2022-11-18 17:51:29 +0100

    Adding Functions to Evaluation Controller
    Signed-off-by: Ignasi <ignasi.fibla@upc.edu>

--- app/src/main/java/com/yara/raco/model/evaluation/EvaluationController.kt
+++ app/src/main/java/com/yara/raco/model/evaluation/EvaluationController.kt
@@ -1,13 +1,57 @@
 package com.yara.raco.model.evaluation
 
 import android.content.Context
+import androidx.compose.ui.text.font.FontWeight
 import com.yara.raco.database.RacoDatabase
+import com.yara.raco.model.grade.Grade
 
 class EvaluationController private constructor(context: Context) {
 
     private val racoDatabase = RacoDatabase.getInstance(context)
 
     fun getEvaluations() = racoDatabase.evaluationDAO.getEvaluations()
+
+    suspend fun getEvaluation(id: Int) = racoDatabase.evaluationDAO.getEvaluation(id)
+
+    suspend fun getEvaluationIds() = racoDatabase.evaluationDAO.getEvaluationIds()
+
+    suspend fun insertEvaluation(evaluation: Evaluation) =
+        racoDatabase.evaluationDAO.insertEvaluation(evaluation)
+
+    suspend fun deleteEvaluation(evaluation: Evaluation) =
+        racoDatabase.evaluationDAO.deleteEvaluation(evaluation)
+
+    suspend fun deleteEvaluation(id: Int) = racoDatabase.evaluationDAO.deleteEvaluation(id)
+
+    suspend fun deleteAllEvaluations() = racoDatabase.evaluationDAO.deleteAllEvaluations()
+
+    suspend fun addGradeToEvaluation(grade: Grade, evaluation: Evaluation) {
+        evaluation.listOfGrade.add(grade)
+        insertEvaluation(evaluation)
+    }
+
+    suspend fun removeGradeFromEvaluation(grade: Grade, evaluation: Evaluation) {
+        evaluation.listOfGrade.remove(grade)
+        insertEvaluation(evaluation)
+    }
+
+    suspend fun updateMarkFromGradeInEvaluation(
+        gradeId: String,
+        evaluation: Evaluation,
+        mark: Double
+    ) {
+        evaluation.listOfGrade.find { it.id == gradeId }?.mark = mark
+        insertEvaluation(evaluation)
+    }
+
+    suspend fun updateWeightFromGradeInEvaluation(
+        gradeId: String,
+        evaluation: Evaluation,
+        weight: Double
+    ) {
+        evaluation.listOfGrade.find { it.id == gradeId }?.weight = weight
+        insertEvaluation(evaluation)
+    }
 
     companion object {
         private var INSTANCE: EvaluationController? = null
