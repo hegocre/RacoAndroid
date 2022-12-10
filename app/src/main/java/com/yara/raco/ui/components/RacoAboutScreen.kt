4dbe1d968a7e53dddae7fb6549cd4a49dbbb3cfa
hegocre <15657088+hegocre@users.noreply.github.com>
2022-12-10 22:07:19 +0100

    Add licenses and privacy

--- app/src/main/java/com/yara/raco/ui/components/RacoAboutScreen.kt
+++ app/src/main/java/com/yara/raco/ui/components/RacoAboutScreen.kt
@@ -1,35 +1,44 @@
 package com.yara.raco.ui.components
 
-import android.content.Intent
-import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.yara.raco.R
 import com.yara.raco.ui.theme.RacoTheme
+
+data class LicenseNotice(
+    val name: String,
+    val copyright: String,
+    val licenseName: String,
+    val licenseUrl: String
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RacoAboutScreen(
     onBackPressed: () -> Unit
 ) {
-    val context = LocalContext.current
+    val uriHandler = LocalUriHandler.current
+
+    var showLicensesDialog by rememberSaveable { mutableStateOf(false) }
 
     RacoTheme {
         Scaffold(
@@ -102,11 +111,7 @@ fun RacoAboutScreen(
                                         )
                                     },
                                     primaryText = { Text(text = stringResource(id = R.string.source_code)) },
-                                    onClick = {
-                                        val intent = Intent(Intent.ACTION_VIEW)
-                                        intent.data = Uri.parse(repoUrl)
-                                        context.startActivity(intent)
-                                    }
+                                    onClick = { uriHandler.openUri(repoUrl) }
                                 )
 
                                 AboutTextField(
@@ -117,11 +122,7 @@ fun RacoAboutScreen(
                                         )
                                     },
                                     primaryText = { Text(text = stringResource(id = R.string.help_suggestions)) },
-                                    onClick = {
-                                        val intent = Intent(Intent.ACTION_VIEW)
-                                        intent.data = Uri.parse("$repoUrl/issues")
-                                        context.startActivity(intent)
-                                    }
+                                    onClick = { uriHandler.openUri("$repoUrl/issues") }
                                 )
 
                                 AboutTextField(
@@ -132,7 +133,19 @@ fun RacoAboutScreen(
                                         )
                                     },
                                     primaryText = { Text(text = stringResource(id = R.string.licenses)) },
-                                    onClick = {}
+                                    onClick = { showLicensesDialog = true }
+                                )
+
+                                val policyUrl = stringResource(id = R.string.privacy_policy_url)
+                                AboutTextField(
+                                    icon = {
+                                        Icon(
+                                            imageVector = Icons.Outlined.Policy,
+                                            contentDescription = stringResource(id = R.string.privacy_policy)
+                                        )
+                                    },
+                                    primaryText = { Text(text = stringResource(id = R.string.privacy_policy)) },
+                                    onClick = { uriHandler.openUri(policyUrl) }
                                 )
                             }
                         }
@@ -164,11 +177,7 @@ fun RacoAboutScreen(
                                             )
                                         },
                                         primaryText = { Text(text = author.key) },
-                                        onClick = {
-                                            val intent = Intent(Intent.ACTION_VIEW)
-                                            intent.data = Uri.parse(author.value)
-                                            context.startActivity(intent)
-                                        }
+                                        onClick = { uriHandler.openUri(author.value) }
                                     )
                                 }
                             }
@@ -180,11 +189,14 @@ fun RacoAboutScreen(
                     style = MaterialTheme.typography.bodySmall,
                     modifier = Modifier
                         .padding(all = 4.dp)
-                        .clickable {
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.data = Uri.parse(apiUrl)
-                            context.startActivity(intent)
-                        }
+                        .clickable { uriHandler.openUri(apiUrl) }
+                )
+            }
+
+            if (showLicensesDialog) {
+                LicensesDialog(
+                    licenses = licenses,
+                    onDismissRequest = { showLicensesDialog = false }
                 )
             }
         }
@@ -231,12 +243,104 @@ fun AboutTextField(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LicensesDialog(
+    licenses: List<LicenseNotice>,
+    onDismissRequest: (() -> Unit)? = null
+) {
+
+    Dialog(
+        onDismissRequest = { onDismissRequest?.invoke() },
+    ) {
+        Surface(
+            color = MaterialTheme.colorScheme.surface,
+            contentColor = contentColorFor(backgroundColor = MaterialTheme.colorScheme.surface),
+            shape = MaterialTheme.shapes.extraLarge,
+        ) {
+            Column(modifier = Modifier.padding(vertical = 24.dp)) {
+                Text(
+                    text = stringResource(id = R.string.licenses),
+                    style = MaterialTheme.typography.headlineSmall,
+                    modifier = Modifier
+                        .padding(bottom = 16.dp)
+                        .padding(horizontal = 24.dp)
+                )
+
+                val uriHandler = LocalUriHandler.current
+
+                LazyColumn {
+                    items(items = licenses, key = { it.name }) { license ->
+                        ListItem(
+                            headlineText = {
+                                Text(text = license.name)
+                            },
+                            overlineText = {
+                                Text(text = license.licenseName)
+                            },
+                            supportingText = {
+                                Text(text = license.copyright)
+                            },
+                            modifier = Modifier
+                                .clickable {
+                                    uriHandler.openUri(license.licenseUrl)
+                                }
+                                .padding(horizontal = 10.dp)
+                        )
+                    }
+                }
+
+                TextButton(
+                    onClick = { onDismissRequest?.invoke() },
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(horizontal = 24.dp)
+                ) {
+                    Text(text = stringResource(android.R.string.ok))
+                }
+            }
+        }
+    }
+}
+
 const val apiUrl = "https://api.fib.upc.edu/v2/"
 const val repoUrl = "https://github.com/hegocre/RacoAndroid"
 val authors = mapOf(
     "Oriol Deiros" to "https://github.com/oriol366",
     "Ignasi Fibla" to "https://github.com/IFibla",
     "Hector Godoy" to "https://github.com/hegocre",
+)
+val licenses = listOf(
+    LicenseNotice(
+        name = "Kotlin Programming Language",
+        copyright = "Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.",
+        licenseName = "Apache License 2.0",
+        licenseUrl = "https://github.com/JetBrains/kotlin/blob/master/license/LICENSE.txt"
+    ),
+    LicenseNotice(
+        name = "Android Jetpack",
+        copyright = "Copyright 2020 Google LLC",
+        licenseName = "Apache License 2.0",
+        licenseUrl = "https://github.com/androidx/androidx/blob/androidx-main/LICENSE.txt"
+    ),
+    LicenseNotice(
+        name = "Accompanist",
+        copyright = "Copyright 2020 The Android Open Source Project",
+        licenseName = "Apache License 2.0",
+        licenseUrl = "https://github.com/google/accompanist/blob/main/LICENSE"
+    ),
+    LicenseNotice(
+        name = "HtmlText",
+        copyright = "Copyright 2021 Alexander Karkossa",
+        licenseName = "Apache License 2.0",
+        licenseUrl = "https://github.com/ch4rl3x/HtmlText/blob/main/LICENSE"
+    ),
+    LicenseNotice(
+        name = "OkHttp",
+        copyright = "Copyright 2019 Square, Inc.",
+        licenseName = "Apache License 2.0",
+        licenseUrl = "https://github.com/square/okhttp/blob/master/LICENSE.txt"
+    )
 )
 
 @Preview
