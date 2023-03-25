1529ac00b69e8053d1e203b694e89a95e7aafba1
hegocre <15657088+hegocre@users.noreply.github.com>
2023-03-25 14:30:16 +0100

    Remove back arrow animation

--- app/src/main/java/com/yara/raco/ui/components/RacoMainTopAppBar.kt
+++ app/src/main/java/com/yara/raco/ui/components/RacoMainTopAppBar.kt
@@ -1,24 +1,18 @@
 package com.yara.raco.ui.components
 
 import androidx.compose.animation.*
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.IntOffset
 import com.yara.raco.R
-import com.yara.raco.ui.components.tooltip.tooltip
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,12 +33,8 @@ fun RacoMainTopAppBar(
         scrollBehavior = scrollBehavior,
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(scrolledContainerColor = MaterialTheme.colorScheme.surface),
         navigationIcon = {
-            AnimatedVisibility(
-                visible = onBackPress != null,
-                enter = slideIn(tween(), initialOffset = { offset -> IntOffset(-offset.width, 0) }),
-                exit = slideOut(tween(), targetOffset = { offset -> IntOffset(-offset.width, 0) })
-            ) {
-                IconButton(onClick = { onBackPress?.invoke() }) {
+            if (onBackPress != null) {
+                IconButton(onClick = onBackPress) {
                     Icon(
                         imageVector = Icons.Default.ArrowBack, contentDescription = stringResource(
                             id = R.string.back
